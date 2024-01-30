48fee373a06f61a93591f84b161943bd2dbfb31e
saikrishna817 <saikrishnaganji817@gmail.com>
2024-01-30 15:55:27 +0530

    topbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ import { CartComponent } from './pages/cart/cart.component';
 import { FaqsComponent } from './faqs/faqs.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
   { path: 'menu', component: MenuComponent },
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
