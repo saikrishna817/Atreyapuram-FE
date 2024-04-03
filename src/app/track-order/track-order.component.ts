233c4da5a141fc40b6c5e15e19814b1a6f7cb8e7
SoftechIn <sathyasoftechin@gmail.com>
2024-04-03 11:03:45 +0530

    UI

--- src/app/track-order/track-order.component.ts
+++ src/app/track-order/track-order.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { environment } from '../../environments/environment.prod';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { UserService } from '../sharepage/navbar/navbar.service';
 
 @Component({
   selector: 'app-track-order',
@@ -7,14 +10,37 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./track-order.component.css']
 })
 export class TrackOrderComponent implements OnInit {
-  orderId:any;
+  orderId: any;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute,
+    private http: HttpClient,
+    private userService: UserService,) { }
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
       this.orderId = params['orderId'];
-      console.log(this.orderId,'orderr id')
+      console.log(this.orderId, 'orderr id')
     });
+    this.getTrackOrderDetails()
+  }
+
+  getTrackOrderDetails() {
+    const userId = this.userService.getLoggedInUserId();
+    const postData = {
+      product: {
+        orderId: this.orderId,
+        userId:userId,
+        date:''
+      }
+    };
+    const apiUrl = environment.placeOrder;
+    this.http.post(apiUrl, postData).subscribe(
+      (res: any) => {
+        console.log(res);
+      },
+      (err: any) => {
+        console.error(err, 'errorrr');
+      }
+    );
   }
 }
